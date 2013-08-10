8618596124079e98a882058502d9df13b546095f
Nikolay Stanchenko <nick.stanch@gmail.com>
2013-08-10 22:51:45 +0100

    ! Tweak MediaQuery API

--- src/main/scala/org/macroid/MediaQueries.scala
+++ src/main/scala/org/macroid/MediaQueries.scala
@@ -6,8 +6,8 @@ import android.view.WindowManager
 import scalaz.Monoid
 
 case class MediaQuery(b: Boolean) {
-  def apply[A](v: A) = if (b) Some(v) else None
-  def ?[A: Monoid](v: A) = if (b) v else implicitly[Monoid[A]].zero
+  def ?[A](v: A) = if (b) Some(v) else None
+  def ?![A: Monoid](v: A) = if (b) v else implicitly[Monoid[A]].zero
   def &(q: MediaQuery) = b && q.b
   def |(q: MediaQuery) = b || q.b
 }

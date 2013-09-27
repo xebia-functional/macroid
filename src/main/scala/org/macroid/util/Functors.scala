c68aa7477883a9675012bbb3865e9c40da5ded25
Nikolay Stanchenko <nick.stanch@gmail.com>
2013-09-28 00:11:12 +0100

    + Change slots to be just options, unrivaled compartmentalization

--- src/main/scala/org/macroid/util/Functors.scala
+++ src/main/scala/org/macroid/util/Functors.scala
@@ -3,8 +3,11 @@ package org.macroid.util
 import scala.concurrent.{ Future, ExecutionContext }
 import scalaz.Functor
 import io.dylemma.frp.{ EventStream, Observer }
+import rx.Rx
+import org.macroid.LayoutBuilding
+import android.view.View
 
-object Functors {
+trait Functors {
   implicit val listF = scalaz.std.list.listInstance
   implicit val optionF = scalaz.std.option.optionInstance
   implicit def futureF(implicit ec: ExecutionContext) = new Functor[Future] {
@@ -13,4 +16,9 @@ object Functors {
   implicit def eventStreamF(implicit ob: Observer) = new Functor[EventStream] {
     def map[A, B](fa: EventStream[A])(f: A ⇒ B) = fa.map(f)
   }
+  implicit object rxF extends Functor[Rx] {
+    def map[A, B](fa: Rx[A])(f: A ⇒ B) = fa.map(f)
+  }
 }
+
+object Functors extends Functors

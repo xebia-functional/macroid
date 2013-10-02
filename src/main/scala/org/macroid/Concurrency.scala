db9eeed350cf766c634d594433f1d7d7ade17a1c
Nikolay Stanchenko <nick.stanch@gmail.com>
2013-10-02 20:50:28 +0100

    + Fix recoverUi, remove collectUi

--- src/main/scala/org/macroid/Concurrency.scala
+++ src/main/scala/org/macroid/Concurrency.scala
@@ -45,23 +45,9 @@ trait Concurrency {
     def mapUi[S](f: Function[A, S])(implicit ec: ExecutionContext): Future[S] = {
       value flatMap (x ⇒ runOnUiThread(f(x)))
     }
-    /** Same as collect, but performed on UI thread */
-    def collectUi[S](pf: PartialFunction[A, S])(implicit ec: ExecutionContext) {
-      val uiPromise = Promise[S]()
-      value collect {
-        case x if pf.isDefinedAt(x) ⇒ uiPromise.completeWith(runOnUiThread(pf(x)))
-        case _ ⇒ uiPromise.complete(Failure(new NoSuchElementException))
-      }
-      uiPromise.future
-    }
     /** Same as recover, but performed on UI thread */
     def recoverUi[U >: A](pf: PartialFunction[Throwable, U])(implicit ec: ExecutionContext): Future[U] = {
-      val uiPromise = Promise[U]()
-      value recover {
-        case t if pf.isDefinedAt(t) ⇒ uiPromise.completeWith(runOnUiThread(pf(t)))
-        case _ ⇒ uiPromise.completeWith(value)
-      }
-      uiPromise.future
+      value recoverWith { case t if pf.isDefinedAt(t) ⇒ runOnUiThread(pf(t)) }
     }
   }
 }

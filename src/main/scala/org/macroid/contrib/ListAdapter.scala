1e81e17d84df498c88bbd9d7b0a8ce30a227f3b4
Nikolay Stanchenko <nick.stanch@gmail.com>
2013-10-31 00:46:08 +0000

    ! ListAdapter.fillView now receives view parent

--- src/main/scala/org/macroid/contrib/ListAdapter.scala
+++ src/main/scala/org/macroid/contrib/ListAdapter.scala
@@ -10,26 +10,26 @@ import scala.collection.JavaConversions._
 abstract class ListAdapter[A, B <: View](implicit ctx: Context) extends ArrayAdapter[A](ctx, 0) {
   override def getView(position: Int, view: View, parent: ViewGroup): View = {
     val v = Option(view).flatMap(x ⇒ Try(x.asInstanceOf[B]).toOption).getOrElse(makeView)
-    fillView(v, getItem(position)); v
+    fillView(v, parent, getItem(position)); v
   }
   def makeView: B
-  def fillView(view: B, data: A): Any
+  def fillView(view: B, parent: ViewGroup, data: A): Any
 }
 
 object ListAdapter extends LayoutDsl {
   def text[A](data: Seq[A])(t: Tweak[TextView], f: A ⇒ Tweak[TextView])(implicit ctx: Context) = new ListAdapter[A, TextView] {
     addAll(data)
     def makeView = new TextView(ctx) ~> t
-    def fillView(view: TextView, data: A) = view ~> f(data)
+    def fillView(view: TextView, parent: ViewGroup, data: A) = view ~> f(data)
   }
   def simple[A, B <: View](data: Seq[A])(v: ⇒ B, f: A ⇒ Tweak[B])(implicit ctx: Context) = new ListAdapter[A, B] {
     addAll(data)
     def makeView = v
-    def fillView(view: B, data: A) = view ~> f(data)
+    def fillView(view: B, parent: ViewGroup, data: A) = view ~> f(data)
   }
   def apply[A, B <: ViewGroup](data: Seq[A])(l: ⇒ B, f: A ⇒ Transformer)(implicit ctx: Context) = new ListAdapter[A, B] {
     addAll(data)
     def makeView = l
-    def fillView(view: B, data: A) = view ~~> f(data)
+    def fillView(view: B, parent: ViewGroup, data: A) = view ~~> f(data)
   }
 }

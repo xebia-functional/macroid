ec5fc327b666e4964cd64fa4fe3063c8246b0aa8
Nikolay Stanchenko <nick.stanch@gmail.com>
2013-11-02 00:30:52 +0000

    ! core: Change FragmentDsl API, new bundle helper, close #12
    ! New FragmentDsl, new bundle helper (close #12)
    
    = Remove shapeless dependency

--- macroid-core/src/main/scala/org/macroid/util/Thunk.scala
+++ macroid-core/src/main/scala/org/macroid/util/Thunk.scala
@@ -2,6 +2,7 @@ package org.macroid.util
 
 class Thunk[+A](v: ⇒ A) extends Function0[A] {
   def apply() = v
+  def map[B >: A](f: A ⇒ B) = Thunk(f(v))
 }
 object Thunk {
   def apply[A](v: ⇒ A) = new Thunk(v)

ca4844a8740d2fe08e435144fbb57141e15bf455
Nikolay Stanchenko <nick.stanch@gmail.com>
2014-03-11 19:43:07 +0000

    + core: Add string resource utility

--- macroid-core/src/main/scala/org/macroid/Resources.scala
+++ macroid-core/src/main/scala/org/macroid/Resources.scala
@@ -4,22 +4,23 @@ import scala.language.dynamics
 import scala.language.experimental.macros
 import scala.reflect.macros.{ Context ⇒ MacroContext }
 
-trait Resources {
+private[macroid] trait Resources {
   import ResourceMacros._
 
   case class Res(R: AnyRef) {
     object string extends Dynamic {
-      def applyDynamic(id: String)(implicit ctx: AppContext): String = macro stringImpl
+      def selectDynamic(id: String)(implicit ctx: AppContext): String = macro stringImpl
     }
   }
 }
+
+object Resources extends Resources
 
 object ResourceMacros {
   def stringImpl(c: MacroContext)(id: c.Expr[String])(ctx: c.Expr[AppContext]) = {
     import c.universe._
     val Expr(Literal(Constant(idValue: String))) = id
-    val R = c.prefix.tree
-    ???
-    c.Expr[String](q"$ctx.get.getResources.getString($R.string.${newTermName(idValue)})")
+    val Select(Apply(_, List(r)), _) = c.prefix.tree
+    c.Expr[String](q"$ctx.get.getResources.getString($r.string.${newTermName(idValue)})")
   }
 }

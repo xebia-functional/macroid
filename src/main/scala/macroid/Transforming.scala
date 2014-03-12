5b6e59cdb7cb6472a9975faa5ff5e3f1b55948dd
Nikolay Stanchenko <nick.stanch@gmail.com>
2014-03-12 23:34:37 +0000

    ! Rewrite transforming in the way of tweaking/snailing

--- src/main/scala/macroid/Transforming.scala
+++ src/main/scala/macroid/Transforming.scala
@@ -26,11 +26,27 @@ object Layout {
   }
 }
 
+trait CanTransform[L, T, R] {
+  def transform(l: L, t: T): Ui[R]
+}
+
+object CanTransform {
+  implicit def `Layout is transformable`[L <: ViewGroup] =
+    new CanTransform[L, Transformer, L] {
+      def transform(l: L, t: Transformer) = Ui { t(l); l }
+    }
+
+  implicit def `Ui is transformable`[L, T, R](implicit canTransform: CanTransform[L, T, R]) =
+    new CanTransform[Ui[L], T, L] {
+      def transform(ui: Ui[L], t: T) = ui flatMap { l ⇒ canTransform.transform(l, t).map(_ ⇒ l) }
+    }
+}
+
 private[macroid] trait Transforming {
   /** Transforming operator */
-  implicit class TransformingOps[L <: ViewGroup](l: L) {
-    /** Apply transformer */
-    def ~~>(t: Transformer) = Ui { t(l); l }
+  implicit class TransformingOps[L](l: L) {
+    /** Apply a transformer */
+    def ~~>[T, R](t: T)(implicit canTransform: CanTransform[L, T, R]) = canTransform.transform(l, t)
   }
 }
 

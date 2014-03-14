8b8c926f24c5d0e343eb80266838e4d86180ff8d
Nikolay Stanchenko <nick.stanch@gmail.com>
2014-03-14 00:30:19 +0000

    ! core: Reverse the arrows in Dialog and Toast APIs

--- macroid-core/src/main/scala/macroid/DialogDsl.scala
+++ macroid-core/src/main/scala/macroid/DialogDsl.scala
@@ -90,8 +90,8 @@ private[macroid] trait Phrasing extends DialogImplicits {
   object create
 
   implicit class PhrasingOps(dialog: Ui[AlertDialog.Builder]) {
-    def ~>(phrase: Phrase) = dialog map { d ⇒ phrase(d); d }
-    def ~>(c: create.type) = dialog.map(_.create())
+    def <~(phrase: Phrase) = dialog map { d ⇒ phrase(d); d }
+    def <~(c: create.type) = dialog.map(_.create())
   }
 }
 

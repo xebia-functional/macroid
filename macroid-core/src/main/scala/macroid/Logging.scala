886426633b4c59ea83c76c350dfe6496ee4f3007
Nikolay Stanchenko <nick.stanch@gmail.com>
2014-04-09 15:44:52 +0100

    = core: Update build, fix a couple of bugs

--- macroid-core/src/main/scala/macroid/Logging.scala
+++ macroid-core/src/main/scala/macroid/Logging.scala
@@ -6,12 +6,12 @@ case class LogTag(tag: String)
 
 private[macroid] trait Logging {
   implicit class LoggingStringContext(sc: StringContext) {
-    def logV(args: Any*) = new LogWriter(sc.s(args), Log.v)
-    def logI(args: Any*) = new LogWriter(sc.s(args), Log.i)
-    def logD(args: Any*) = new LogWriter(sc.s(args), Log.d)
-    def logW(args: Any*) = new LogWriter(sc.s(args), Log.w)
-    def logE(args: Any*) = new LogWriter(sc.s(args), Log.e)
-    def logWtf(args: Any*) = new LogWriter(sc.s(args), Log.wtf)
+    def logV(args: Any*) = new LogWriter(sc.s(args: _*), Log.v)
+    def logI(args: Any*) = new LogWriter(sc.s(args: _*), Log.i)
+    def logD(args: Any*) = new LogWriter(sc.s(args: _*), Log.d)
+    def logW(args: Any*) = new LogWriter(sc.s(args: _*), Log.w)
+    def logE(args: Any*) = new LogWriter(sc.s(args: _*), Log.e)
+    def logWtf(args: Any*) = new LogWriter(sc.s(args: _*), Log.wtf)
   }
 
   class LogWriter(msg: String, f: (String, String) ⇒ Int) {

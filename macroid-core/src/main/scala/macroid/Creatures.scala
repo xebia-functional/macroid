b925e136339c192e26013ad07b0d796c47247915
Nikolay Stanchenko <nick.stanch@gmail.com>
2014-07-04 22:10:31 +0100

    ! core: Refactor and explode contrib.ExtraTweaks, turn creatures into value classes

--- macroid-core/src/main/scala/macroid/Creatures.scala
+++ macroid-core/src/main/scala/macroid/Creatures.scala
@@ -5,7 +5,7 @@ import scala.concurrent.Future
 import macroid.util.Ui
 
 /** A Tweak is something that mutates a widget */
-case class Tweak[-W <: View](f: W ⇒ Unit) {
+case class Tweak[-W <: View](f: W ⇒ Unit) extends AnyVal {
   def apply(w: W) = f(w)
 
   /** Combine (sequence) with another tweak */
@@ -27,7 +27,7 @@ object Tweak {
 }
 
 /** A snail mutates the view slowly (e.g. animation) */
-case class Snail[-W <: View](f: W ⇒ Future[Unit]) {
+case class Snail[-W <: View](f: W ⇒ Future[Unit]) extends AnyVal {
   import UiThreading._
 
   def apply(w: W) = f(w)
@@ -50,7 +50,7 @@ object Snail {
   def blank[W <: View] = Snail[W](_ ⇒ Future.successful(()))
 }
 
-case class Transformer(f: PartialFunction[View, Ui[Any]]) {
+case class Transformer(f: PartialFunction[View, Ui[Any]]) extends AnyVal {
   def apply(w: View): Unit = {
     f.lift.apply(w).foreach(_.get)
     w match {
--- macroid-core/src/main/scala/macroid/contrib/ExtraTweaks.scala
+++ macroid-core/src/main/scala/macroid/contrib/ExtraTweaks.scala
@@ -1,40 +1,67 @@
 package macroid.contrib
 
-import macroid.{ Tweak, AppContext }
-import android.widget.{ ImageView, TextView }
 import android.graphics.{ Bitmap, Typeface }
 import android.util.TypedValue
-import android.view.View
+import android.view.ViewGroup.LayoutParams._
+import android.view.{ View, ViewGroup }
+import android.widget._
+import macroid.Tweak
 
-private[macroid] trait ExtraTweaks {
-  object TextStyle {
-    val bold = Tweak[TextView](x ⇒ x.setTypeface(x.getTypeface, Typeface.BOLD))
-    val italic = Tweak[TextView](x ⇒ x.setTypeface(x.getTypeface, Typeface.ITALIC))
-    val boldItalic = Tweak[TextView](x ⇒ x.setTypeface(x.getTypeface, Typeface.BOLD_ITALIC))
-  }
+/** Extra tweaks for TextView */
+object TextTweaks {
+  type W = TextView
 
-  object TextSize {
-    def medium(implicit ctx: AppContext) = Tweak[TextView](_.setTextAppearance(ctx.get, android.R.style.TextAppearance_Medium))
-    def large(implicit ctx: AppContext) = Tweak[TextView](_.setTextAppearance(ctx.get, android.R.style.TextAppearance_Large))
-    def sp(points: Int) = Tweak[TextView](_.setTextSize(TypedValue.COMPLEX_UNIT_SP, points))
-  }
+  val bold = Tweak[W](x ⇒ x.setTypeface(x.getTypeface, Typeface.BOLD))
+  val italic = Tweak[W](x ⇒ x.setTypeface(x.getTypeface, Typeface.ITALIC))
+  val boldItalic = Tweak[W](x ⇒ x.setTypeface(x.getTypeface, Typeface.BOLD_ITALIC))
 
-  object TextFace {
-    val serif = Tweak[TextView](x ⇒ x.setTypeface(Typeface.SERIF, Option(x.getTypeface).map(_.getStyle).getOrElse(Typeface.NORMAL)))
-    val sans = Tweak[TextView](x ⇒ x.setTypeface(Typeface.SANS_SERIF, Option(x.getTypeface).map(_.getStyle).getOrElse(Typeface.NORMAL)))
-    val mono = Tweak[TextView](x ⇒ x.setTypeface(Typeface.MONOSPACE, Option(x.getTypeface).map(_.getStyle).getOrElse(Typeface.NORMAL)))
-  }
+  val serif = Tweak[W](x ⇒ x.setTypeface(Typeface.SERIF, Option(x.getTypeface).map(_.getStyle).getOrElse(Typeface.NORMAL)))
+  val sans = Tweak[W](x ⇒ x.setTypeface(Typeface.SANS_SERIF, Option(x.getTypeface).map(_.getStyle).getOrElse(Typeface.NORMAL)))
+  val mono = Tweak[W](x ⇒ x.setTypeface(Typeface.MONOSPACE, Option(x.getTypeface).map(_.getStyle).getOrElse(Typeface.NORMAL)))
 
-  object Bg {
-    def res(id: Int) = Tweak[View](_.setBackgroundResource(id))
-    def resource(id: Int) = Tweak[View](_.setBackgroundResource(id))
-    def color(color: Int) = Tweak[View](_.setBackgroundColor(color))
-  }
-
-  object Image {
-    def bitmap(bitmap: Bitmap) = Tweak[ImageView](_.setImageBitmap(bitmap))
-    def adjustBounds = Tweak[ImageView](_.setAdjustViewBounds(true))
-  }
+  def size(points: Int) = Tweak[W](_.setTextSize(TypedValue.COMPLEX_UNIT_SP, points))
+  val mediumSize = size(18)
+  val largeSize = size(22)
 }
 
-object ExtraTweaks extends ExtraTweaks
+/** Extra tweaks for ImageView */
+object ImageTweaks {
+  type W = ImageView
+
+  def bitmap(bitmap: Bitmap) = Tweak[W](_.setImageBitmap(bitmap))
+  val adjustBounds = Tweak[W](_.setAdjustViewBounds(true))
+}
+
+/** Extra tweaks for ListView */
+object ListTweaks {
+  type W = ListView
+
+  val noDivider = Tweak[W](_.setDivider(null))
+  def adapter(adapter: ListAdapter) = Tweak[W](_.setAdapter(adapter))
+}
+
+/** Extra tweaks for backgrounds */
+object BgTweaks {
+  type W = View
+
+  def res(id: Int) = Tweak[W](_.setBackgroundResource(id))
+  def color(color: Int) = Tweak[W](_.setBackgroundColor(color))
+}
+
+/** Extra tweaks for SeekBar */
+object SeekTweaks {
+  type W = SeekBar
+
+  def seek(p: Int) = Tweak[SeekBar](_.setProgress(p))
+}
+
+/** Extra layout params */
+object LpTweaks {
+  import macroid.Tweaks._
+  type W = View
+
+  val matchParent = lp[ViewGroup](MATCH_PARENT, MATCH_PARENT)
+  val wrapContent = lp[ViewGroup](WRAP_CONTENT, WRAP_CONTENT)
+  val matchWidth = lp[ViewGroup](MATCH_PARENT, WRAP_CONTENT)
+  val matchHeight = lp[ViewGroup](WRAP_CONTENT, MATCH_PARENT)
+}

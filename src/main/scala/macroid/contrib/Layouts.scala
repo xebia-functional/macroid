d759154e084eb73e121371e2b6a4ddc4b2914d4e
Darren Gibson <zarthross@gmail.com>
2014-07-12 19:10:47 -0500

    Added SmartRelativeLayout

--- src/main/scala/macroid/contrib/Layouts.scala
+++ src/main/scala/macroid/contrib/Layouts.scala
@@ -1,6 +1,6 @@
 package macroid.contrib
 
-import android.widget.{ GridLayout, LinearLayout }
+import android.widget.{ GridLayout, LinearLayout, RelativeLayout }
 import android.content.Context
 
 object Layouts {
@@ -10,5 +10,19 @@ object Layouts {
 
   class HorizontalLinearLayout(ctx: Context) extends LinearLayout(ctx) {
     setOrientation(LinearLayout.HORIZONTAL)
+  }
+}
+
+/** Smart Relative Layout params */
+class SmartRelativeLayout(ctx: Context) extends RelativeLayout(ctx)
+object SmartRelativeLayout {
+  // Android uses -1 to indicate no anchor so we are using it
+  // instead of Option to make writing rules cleaner
+  case class Rule(verb: Int, anchor: Int = -1)
+  class LayoutParams(w: Int, h: Int, rules: Rule*) extends RelativeLayout.LayoutParams(w, h) {
+    rules foreach {
+      case Rule(verb, -1) ⇒ addRule(verb)
+      case Rule(verb, anchor) ⇒ addRule(verb, anchor)
+    }
   }
 }

ef1958176e38c37fc2030e289b581d350488476e
rick <contou.y@gmail.com>
2017-08-09 23:09:32 +0800

    + extras: DSL for navigating between activities #62

--- macroid-extras/src/main/scala/macroid/extras/UIActionsExtras.scala
+++ macroid-extras/src/main/scala/macroid/extras/UIActionsExtras.scala
@@ -1,9 +1,10 @@
 package macroid.extras
 
-import android.content.Context
-import android.os.{ Handler, Vibrator }
+import android.app.Activity
+import android.content.{ Context, Intent }
+import android.os.{ Bundle, Handler, Parcelable, Vibrator }
 import android.widget.Toast
-import macroid.{ ContextWrapper, Ui }
+import macroid.{ ActivityContextWrapper, ContextWrapper, Ui }
 
 object UIActionsExtras {
 
@@ -37,6 +38,74 @@ object UIActionsExtras {
     contextWrapper.application.getSystemService(Context.VIBRATOR_SERVICE) match {
       case vibrator: Vibrator ⇒ vibrator.vibrate(millis)
       case _ ⇒
+    }
+  }
+
+  private implicit class IntentVisitor(intent: Intent) {
+    def withExtra(key: String, value: Any): Unit = {
+      value match {
+        case v: Short ⇒ intent.putExtra(key, v)
+        case v: Int ⇒ intent.putExtra(key, v)
+        case v: Byte ⇒ intent.putExtra(key, v)
+        case v: Double ⇒ intent.putExtra(key, v)
+        case v: Float ⇒ intent.putExtra(key, v)
+        case v: Long ⇒ intent.putExtra(key, v)
+        case v: String ⇒ intent.putExtra(key, v)
+        case v: Boolean ⇒ intent.putExtra(key, v)
+        case v: Char ⇒ intent.putExtra(key, v)
+
+        // this will only fail when item type is not primitive or Serializable or Parcelable
+        case v: Array[_] ⇒ intent.putExtra(key, v)
+
+        case v: CharSequence ⇒ intent.putExtra(key, v)
+        case v: Serializable ⇒ intent.putExtra(key, v)
+        case v: Bundle ⇒ intent.putExtra(key, v)
+        case v: Parcelable ⇒ intent.putExtra(key, v)
+        // Vector and List works fine, because they are Serializable.
+        case v: Seq[_] ⇒ throw new IllegalArgumentException(s"class: ${v.getClass} is not supported, could you use Array instead")
+        case v ⇒ throw new IllegalArgumentException(s"class: ${v.getClass} is not supported")
+      }
+    }
+  }
+
+  private def intentFor(cls: Class[_])(implicit context: ContextWrapper): Intent = {
+    new Intent(context.getOriginal, cls)
+  }
+
+  implicit class activityClassMethod(cls: Class[_ <: Activity]) {
+
+    def withExtras(extras: (String, Any)*)(implicit context: ContextWrapper) = {
+      intentFor(cls).withExtras(extras: _*)
+    }
+
+    def withFlag(flag: Int)(implicit context: ContextWrapper) = {
+      intentFor(cls).addFlags(flag)
+    }
+
+    def startActivity()(implicit context: ContextWrapper): Ui[Unit] = {
+      intentFor(cls).startActivity()
+    }
+
+    def startActivityForResult(requestCode: Int, bundle: Bundle = null)(implicit context: ActivityContextWrapper): Ui[Unit] = {
+      intentFor(cls).startActivityForResult(requestCode, bundle)
+    }
+  }
+
+  implicit class intentMethod(intent: Intent) {
+    def withExtras(extras: (String, Any)*): Intent = {
+      extras.foreach({
+        case (key, value) ⇒ intent.withExtra(key, value)
+      })
+      intent
+    }
+
+    def startActivity()(implicit context: ContextWrapper): Ui[Unit] = {
+      Ui(context.getOriginal.startActivity(intent))
+    }
+
+    def startActivityForResult(requestCode: Int, bundle: Bundle = null)(implicit context: ActivityContextWrapper): Ui[Unit] = {
+      val currentActivity = context.getOriginal
+      Ui(currentActivity.startActivityForResult(intent, requestCode, bundle))
     }
   }
 
